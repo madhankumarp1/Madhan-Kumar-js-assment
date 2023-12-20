1eb6eb0d0322164d3a1f6f368fa8e8cffe3d0f08
madhankumarp1 <madhankumarp@gmail.com>
2023-12-20 16:25:59 +0530

    changes

--- Mani.js
+++ Mani.js
@@ -1,10 +1,11 @@
+"use strict"; 
 let submit_button = document.querySelector(".submit_btn");
 let user_Product = document.getElementById("ProductName");
 let user_amount = document.getElementById("Amount");
-
 let add_amount = document.querySelector(".amount_add");
 let add_Product = document.querySelector(".product_add");
 let total_amount_container = document.querySelector(".total");
+let sort_button = document.querySelector(".sort_btn");
 
 let totalAmount = 0;
 
@@ -16,20 +17,19 @@ function transaction(event) {
   let productName = user_Product.value.trim();
   let amount = Number(user_amount.value);
 
+  let invalidProduct = document.getElementById("invaldide_prodect");
+  let invalidAmount = document.getElementById("invaldide_Amount");
 
-  let invalidProductMessage = document.getElementById("invaldide_prodect");
-  let invalidAmountMessage = document.getElementById("invaldide_Amount");
-
-  invalidProductMessage.innerHTML = "";
-  invalidAmountMessage.innerHTML = "";
+  invalidProduct.innerHTML = "";
+  invalidAmount.innerHTML = "";
 
   if (productName === "" || isNaN(amount)) {
     setTimeout(() => {
-      invalidProductMessage.innerHTML = "Invalid Product Name";
-      invalidAmountMessage.innerHTML = "Invalid Amount";
+      invalidProduct.innerHTML = "Invalid Product Name";
+      invalidAmount.innerHTML = "Invalid Amount";
     }, 10);
-
   } 
+  
   else if (amount > 0) 
   {
     let Item_name = document.createElement("ul");
@@ -56,35 +56,35 @@ function transaction(event) {
 
     totalAmount += amount;
 
+// --------------------- removebutton function
 
     remove.addEventListener("click", function () {
+      all.remove();
+      totalAmount -= amount;
 
-      // if (amount>0 ){
+      // totalAmountDiv
 
-      //   else{
-        all.remove();
-        totalAmount -= amount;
-        // totalAmountDiv
-        total_amount_container.innerHTML = "";
-        let totalAmountDiv = document.createElement("div");
-        totalAmountDiv.className = "total";
-        let h2tag = document.createElement("p");
-        h2tag.className = "h2t";
-        let divamount = document.createElement("div");
-        divamount.className = "total_2";
-        let h1tag = document.createElement("p");
-        h1tag.className = "h1t";
-        h2tag.textContent = ` ${totalAmount}`;
-        h1tag.innerHTML = "Total Amount";
-        total_amount_container.appendChild(divamount);
-        total_amount_container.appendChild(totalAmountDiv);
-        totalAmountDiv.appendChild(h1tag);
-        divamount.appendChild(h2tag);
-      // }
-  
+      total_amount_container.innerHTML = "";
+      let totalAmountDiv = document.createElement("div");
+      totalAmountDiv.className = "total";
+      let h2tag = document.createElement("p");
+      h2tag.className = "h2t";
+      let divamount = document.createElement("div");
+      divamount.className = "total_2";
+      let h1tag = document.createElement("p");
+      h1tag.className = "h1t";
+      h2tag.textContent = ` ${totalAmount}`;
+      h1tag.innerHTML = "Total Amount";
+      total_amount_container.appendChild(divamount);
+      total_amount_container.appendChild(totalAmountDiv);
+      totalAmountDiv.appendChild(h1tag);
+      divamount.appendChild(h2tag);
     });
 
-    // totalAmountDiv
+
+
+
+    // --------------------totalAmountDiv
     total_amount_container.innerHTML = "";
     let totalAmountDiv = document.createElement("div");
     totalAmountDiv.className = "total";
@@ -103,12 +103,54 @@ function transaction(event) {
 
     user_Product.value = "";
     user_amount.value = "";
-    
 
-    if (amount<0){
-      total_amount_container.innerHTML = "";
+// --------------sort function
+    sort_button.addEventListener("click", sortItems);
+
+function sortItems() {
+  // console.log(true);
+  var Arr = Array.from(add_Product.children);
+
+  for (var i = 1; i < Arr.length; i++) {
+    for (var j = 0; j < i; j++) {
+      var productNameA = Arr[i].querySelector(".hia").textContent;
+      var productNameB = Arr[j].querySelector(".hia").textContent;
+
+      if (productNameA[i] < productNameB[j]) {
+        var x = Arr[i];
+        Arr[i] = Arr[j];
+        Arr[j] = x;
+      }
     }
+  }
 
-    
+  add_Product.innerHTML = "";
+  
+  Arr.forEach((item) => {
+    add_Product.appendChild(item);
+  });
+
+  total_amount_container.innerHTML = "";
+  let totalAmountDiv = document.createElement("div");
+  totalAmountDiv.className = "total";
+  let h2tag = document.createElement("p");
+  h2tag.className = "h2t";
+  let divamount = document.createElement("div");
+  divamount.className = "total_2";
+  let h1tag = document.createElement("p");
+  h1tag.className = "h1t";
+  h2tag.textContent = ` ${totalAmount}`;
+  h1tag.innerHTML = "Total Amount";
+  total_amount_container.appendChild(divamount);
+  total_amount_container.appendChild(totalAmountDiv);
+  totalAmountDiv.appendChild(h1tag);
+  divamount.appendChild(h2tag);
+
+  user_Product.value = "";
+  user_amount.value = "";
+
+
+}
   }
 }
+
